00362488c9411f6bfe07abf0a6a24dceab8b449b
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-03-17 19:04:05 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const manager = data.employees.some((item)=>item.managers.includes(id));
+  return manager;
 }
+console.log( isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+   if(!isManager(managerId)){
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+   }
+   const person = data.employees.filter((elemento)=> elemento.managers.includes(managerId)).map((item) =>`${item.firstName} ${item.lastName}`);
+   return person;
+};
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
