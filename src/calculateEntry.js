e308f915a7420093af433a49dec4908b276504b3
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-03-22 11:36:48 -0300

    requisito 5 e 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,47 @@
 const data = require('../data/zoo_data');
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
 function countEntrants(entrants) {
-  // seu código aqui  lorena
+  const menor = entrants.filter((item) => (item.age < 18)).length
+  const maior = entrants.filter((item) => (item.age >= 18 && item.age < 50)).length;
+  const tercera = entrants.filter((item) => (item.age >= 50)).length;
+  
+  return { child:menor , adult:maior , senior:tercera};
 }
 
+console.log(countEntrants( [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_Operators#operadores_operador de negacao !
+// objeto vazio neste site https://pt.stackoverflow.com/questions/83588/em-javascript-como-verificar-que-um-objeto-est%C3%A1-vazio-sem-jquery
 function calculateEntry(entrants) {
-  // seu código aqui
+  if(!entrants || Object.keys(entrants).length === 0){
+    return 0;
+  }
+  const pessoa = countEntrants(entrants);
+  //console.log(pessoa)
+  const preco = (pessoa.child * 20.99) + (pessoa.adult * 49.99) + (pessoa.senior * 24.99);
+  //console.log(preco);
+  return preco;
 }
-
+console.log(calculateEntry([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,40 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  //A função reduce do JavaScript serve para iterar sobre um array e utilizar o valor de cada item para criar um objeto final com base em alguma regra. Como o próprio nome da função sugere, ela “reduz” os itens de um vetor a um valor único.
+  //usei um if para verificar se o não for passado nenhum parametro, ele fala que se o parametro for diferente a animal executa a arrow function
+  //operador logico not (Negação lógica) Retorna falso caso o único operando possa ser convertido para verdadeiro; senão, retorna verdadeiro.)
+  //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_Operators#operadores_logicos
+  // olhei o git https://github.com/tryber/sd-020-a-project-zoo-functions/pull/44/files
+  if (!animal) {
+    const qtdAnimal = data.species.reduce((acumulador, elemento) => {
+      acumulador[elemento.name] = elemento.residents.length;
+      return acumulador;
+    }, {});
+    return qtdAnimal;
+  }
+  if(!animal.sex){
+    const specie = species.find((item)=> item.name === animal.specie).residents.length;
+    return specie;
+  }
+
+  const specie = species.find((item)=> item.name === animal.specie);
+  const sex = specie.residents.filter((item) => item.sex === animal.sex).length
+
+ // console.log(animalName);
+// console.log(sex);
+ 
+
+  return sex;
 }
+console.log(countAnimals({ specie: 'elephants'}));
 
 module.exports = countAnimals;
+/*- Se nenhum argumento for passado, retorna um objeto cujo o nome de cada espécie é uma chave desse objeto, e o total de animais dessa espécie é o seu valor;
+  - Com o argumento `{ specie: 'penguins' }`, retorna um número, a quantidade de pinguins no zoológico;
+  - Com o argumento `{ specie: 'giraffes', sex: 'female' }`, retorna um número, a quantidade de girafas do sexo feminino.
+**O que será avaliado**
+  - Sem parâmetros, retorna as espécies e sua quantidade;
+  - Recebendo como parâmetro um objeto com a chave `specie`, retorna um número, a quantidade de animais daquela espécie;
+  - Recebendo como parâmetro um objeto com a chave `specie` e `sex`, retorna um número, a quantidade de animais daquela espécie, no sexo selecionado.*/
